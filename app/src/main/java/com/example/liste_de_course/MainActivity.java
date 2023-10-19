da40cb338187d045c3fb1adfd6c5a35ab4e4e6b9
louis <louis.ruellou@gmail.com>
2023-10-19 17:09:41 +0200

    initial commit

--- app/src/main/java/com/example/liste_de_course/MainActivity.java
+++ app/src/main/java/com/example/liste_de_course/MainActivity.java
@@ -3,12 +3,58 @@ package com.example.liste_de_course;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    protected ArrayAdapter affList(ArrayList<String> departements) {
+        ListView lv = (ListView) findViewById(R.id.liste);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, departements);
+        lv.setAdapter(arrayAdapter);
+        return arrayAdapter;
+    }
+
+
+    private void setupListe() {
+
+    }
+
+    /**
+     * Setup the buttons of the app
+     */
+    private void setButtons() {
+        Button burgerMenu = (Button)findViewById(R.id.burgerMenu);
+        burgerMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button addButton = (Button)findViewById(R.id.addBtn);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
